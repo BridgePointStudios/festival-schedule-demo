a7d01191d36a86f291f66ec46e8f1a32e9e67986
BridgePointStudios <jeremy@bridgepointstudio.com>
2015-03-29 18:56:41 -0500

    added venues section

---
+++ client/templates/venues/venues_list.js
@@ -0,0 +1,6 @@
+Meteor.subscribe("venues");
+Template.venuesList.helpers({
+    venues: function() {
+        return Venues.find({}, {sort: {venueName: 1}});
+    }
+});
--- server/security.js
+++ server/security.js
@@ -1,7 +1,13 @@
 Bands.permit(['insert','update','remove']).apply();
+Venues.permit(['insert','update','remove']).apply();
+
 
 if (Meteor.isServer) {
   Meteor.publish("bands", function () {
     return Bands.find();
   });
+  Meteor.publish("venues", function () {
+    return Venues.find();
+  });
+
 }
