9b176a663b69cb00e12c4e908256984d1e092779
BridgePointStudios <jeremy@bridgepointstudio.com>
2015-03-30 22:32:09 -0500

    added event details

--- client/templates/events/events_list.js
+++ client/templates/events/events_list.js
@@ -1,6 +1,6 @@
 Meteor.subscribe("events");
 Template.eventsList.helpers({
-    bands: function() {
-        return Events.find({}, {sort: {eventDate: 1}});
+    events: function() {
+        return Events.find();
     }
 });
