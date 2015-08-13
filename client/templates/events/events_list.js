c8a9aa240b3ca23f1d1c44d51a1b33b81cb82194
Barbacamanitu <russmbiz@gmail.com>
2015-08-13 16:38:37 -0500

    Added Calendar view

--- client/templates/events/events_list.js
+++ client/templates/events/events_list.js
@@ -4,3 +4,25 @@ Template.eventsList.helpers({
         return Events.find({}, {sort: { eventStartTime: 1 }});
     }
 });
+
+Template.eventsList.rendered = function()
+{
+	//Fires once the page is rendered.
+	myCursor = Events.find();
+	var myEvents = [];
+	myCursor.forEach(function(currentEvent) {
+		var bandName = currentEvent.eventBandName;
+		var venueName = currentEvent.eventVenueName;
+		var eventTitle = bandName + " @ " + venueName;
+
+		var tempEvent = {
+			title: eventTitle,
+			start: moment(currentEvent.eventStartTime),
+			allDay: true
+		}
+		$('#myCalendar').fullCalendar('renderEvent',tempEvent,true);
+	});
+
+	
+	
+}
